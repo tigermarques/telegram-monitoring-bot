00d634a81f67d9fc27006127079b217133b6f609
Joao Marques <jgmarques@kpmg.com>
2019-04-16 23:44:36 +0100

    change file names

--- bot/commands/index.js
+++ bot/commands/index.js
@@ -2,7 +2,7 @@ const echo = require('./echo')
 const getlogs = require('./getlogs')
 const log = require('./log')
 const register = require('./register')
-const registeradmin = require('./registeradmin2')
+const registeradmin = require('./registeradmin')
 const unregister = require('./unregister')
 
 module.exports = {
